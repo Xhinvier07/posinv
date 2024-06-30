549daab1cc7ba8c024bb32d4791a40241905bdeb
Jccc07 <johnchristianpaglinawan@gmail.com>
2024-07-01 07:25:13 +0800

    Comment

--- 404.php
+++ 404.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<!-- 
+Hello Jc
+
+-->
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
