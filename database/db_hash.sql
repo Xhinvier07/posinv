ce4d6abd60dd5b6ef68448941e32f4210a2a4778
Shin <jansendeguzman15@gmail.com>
2024-06-30 20:51:39 +0800

    chore: Remove unnecessary files and update database structure

--- database/db_hash.sql
+++ database/db_hash.sql
@@ -1,6 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 30, 2024 at 02:39 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_hash`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
 
 CREATE TABLE `history` (
   `id` int(11) NOT NULL,
@@ -14,16 +39,26 @@ CREATE TABLE `history` (
   `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
 
 CREATE TABLE `inventory` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `product_code` varchar(255) NOT NULL,
+  `product_code` int(11) NOT NULL,
   `stock_in` int(11) NOT NULL,
   `stock_out` int(11) NOT NULL,
-  `created` datetime NOT NULL
+  `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
@@ -34,15 +69,26 @@ CREATE TABLE `products` (
   `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
 
 CREATE TABLE `transaction` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `sales` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
   `discounted_sales` int(11) NOT NULL,
   `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -52,6 +98,21 @@ CREATE TABLE `users` (
   `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `level`, `created`) VALUES
+(4, 'shin', '$2y$10$JehFSfdea/Jwehxj7uP0B.Cblgb0TbTqYJsfa9SLt19b.E7LRvD/G', 0, '2024-06-30 18:45:06'),
+(8, 'user', '$2y$10$POBOCyUpn70mwbNRPkO5L..Go5nOOcz.dgC5v.IaZa.Oyon4h/2fm', 0, '2024-06-30 20:35:16'),
+(9, 'cashier', '$2y$10$jtfZUEXvlur982bTBCJxx.FegYcS/ieNZtoEhlt.lvuGlETgFsGIu', 1, '2024-06-30 20:35:45'),
+(10, 'dane', '$2y$10$0UEsTXLV/HN5b1bpC/5Vk.sHp.q5dOWauLz75uldcREHyNtJyyUzy', 0, '2024-06-30 20:38:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_logs`
+--
 
 CREATE TABLE `user_logs` (
   `id` int(11) NOT NULL,
@@ -59,62 +120,139 @@ CREATE TABLE `user_logs` (
   `type` varchar(255) NOT NULL,
   `sign_in` datetime NOT NULL,
   `sign_out` datetime NOT NULL,
-  `created` datetime NOT NULL
+  `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `user_logs`
+--
 
+INSERT INTO `user_logs` (`id`, `username`, `type`, `sign_in`, `sign_out`, `created`) VALUES
+(1, 'shin', 'Admin', '2024-06-30 18:48:52', '2024-06-30 18:50:34', '2024-06-30 18:48:52'),
+(2, 'shin', 'Admin', '2024-06-30 18:49:06', '2024-06-30 18:50:34', '2024-06-30 18:49:06'),
+(3, 'shin', 'Admin', '2024-06-30 18:49:29', '2024-06-30 18:50:34', '2024-06-30 18:49:29'),
+(4, 'shin', 'Admin', '2024-06-30 18:55:56', '2024-06-30 18:56:03', '2024-06-30 18:55:56'),
+(5, 'shin', 'Admin', '2024-06-30 19:00:59', '2024-06-30 20:19:49', '2024-06-30 19:00:59'),
+(6, 'shin', 'Admin', '2024-06-30 20:11:05', '2024-06-30 20:19:49', '2024-06-30 20:11:05'),
+(7, 'shin', 'Admin', '2024-06-30 20:19:55', '2024-06-30 20:20:04', '2024-06-30 20:19:55'),
+(8, 'shin', 'Admin', '2024-06-30 20:20:09', '0000-00-00 00:00:00', '2024-06-30 20:20:09'),
+(9, 'admin', 'Cashier', '2024-06-30 20:34:56', '2024-06-30 20:35:00', '2024-06-30 20:34:56'),
+(10, 'user', 'Admin', '2024-06-30 20:35:21', '2024-06-30 20:37:11', '2024-06-30 20:35:21'),
+(11, 'shin', 'Admin', '2024-06-30 20:37:15', '0000-00-00 00:00:00', '2024-06-30 20:37:15'),
+(12, 'user', 'Admin', '2024-06-30 20:37:20', '0000-00-00 00:00:00', '2024-06-30 20:37:20'),
+(13, 'cashier', 'Cashier', '2024-06-30 20:37:29', '2024-06-30 20:37:54', '2024-06-30 20:37:29'),
+(14, 'dane', 'Admin', '2024-06-30 20:38:04', '0000-00-00 00:00:00', '2024-06-30 20:38:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `history`
+--
 ALTER TABLE `history`
   ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
   ADD KEY `product_id` (`product_id`);
 
+--
+-- Indexes for table `inventory`
+--
 ALTER TABLE `inventory`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_code` (`product_code`);
 
+--
+-- Indexes for table `products`
+--
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
-
+--
+-- Indexes for table `transaction`
+--
 ALTER TABLE `transaction`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
-
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
-
+--
+-- Indexes for table `user_logs`
+--
 ALTER TABLE `user_logs`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `history`
+--
 ALTER TABLE `history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-
+--
+-- AUTO_INCREMENT for table `inventory`
+--
 ALTER TABLE `inventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-
+--
+-- AUTO_INCREMENT for table `products`
+--
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-
+--
+-- AUTO_INCREMENT for table `transaction`
+--
 ALTER TABLE `transaction`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-
+--
+-- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
-
+--
+-- AUTO_INCREMENT for table `user_logs`
+--
 ALTER TABLE `user_logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
+--
+-- Constraints for dumped tables
+--
 
+--
+-- Constraints for table `history`
+--
 ALTER TABLE `history`
-  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `history_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
+--
+-- Constraints for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `inventory_ibfk_2` FOREIGN KEY (`product_code`) REFERENCES `products` (`id`);
 
+--
+-- Constraints for table `transaction`
+--
 ALTER TABLE `transaction`
   ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
