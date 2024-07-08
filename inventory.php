b289f6698a84c52647368ad63bd0e2b0d55a88c4
Shin <jansendeguzman15@gmail.com>
2024-07-08 18:33:14 +0800

    minimum and maximum

--- inventory.php
+++ inventory.php
@@ -110,10 +110,10 @@
                 <div class="modal-body">
                     <p>Product Information</p>
                     <form class="text-center" action="functions/add-product.php" method="post">
-                        <div class="mb-3"><input class="form-control" type="text" pattern="^(?!\s).*$" name="product_name" placeholder="Product Name" required=""></div>
-                        <div class="mb-3"><input class="form-control" type="number" name="size" placeholder="Size" required=""></div>
-                        <div class="mb-3"><input class="form-control" type="number" name="qty" placeholder="Quantity"></div>
-                        <div class="mb-3"><input class="form-control" type="number" name="price" placeholder="Price" required=""></div>
+                        <div class="mb-3"><input class="form-control" type="text" pattern="^[\w](?!.*?\.{2})[\w.]{1,28}[\w]$" name="product_name" placeholder="Product Name" required=""></div>
+                        <div class="mb-3"><input class="form-control" type="number" name="size" placeholder="Size" min=1 max=999 required=""></div>
+                        <div class="mb-3"><input class="form-control" type="number" name="qty" placeholder="Quantity" min=1 max=999 required=""></div>
+                        <div class="mb-3"><input class="form-control" type="number" name="price" placeholder="Price" min=1 max=999 required=""></div>
                         <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Add Product</button></div>
                     </form>
                 </div>
@@ -131,10 +131,10 @@
                     <p>Product Information</p>
                     <form class="text-center" action="functions/update-product.php"  method="post">
                         <input type="hidden" name="product_id">
-                        <div class="mb-3"><input class="form-control" type="text" pattern="^(?!\s).*$" name="product_name" placeholder="Product Name" disabled></div>
-                        <div class="mb-3"><input class="form-control" type="number" name="size" placeholder="Size" required=""></div>
-                        <div class="mb-3"><input class="form-control" type="number" name="qty" placeholder="Quantity"></div>
-                        <div class="mb-3"><input class="form-control" type="number" name="price" placeholder="Price" required=""></div>
+                        <div class="mb-3"><input class="form-control" type="text" pattern="^[\w](?!.*?\.{2})[\w.]{1,28}[\w]$" name="product_name" placeholder="Product Name" disabled></div>
+                        <div class="mb-3"><input class="form-control" type="number" name="size" placeholder="Size" min=1 max=999 required=""></div>
+                        <div class="mb-3"><input class="form-control" type="number" name="qty" placeholder="Quantity" min=1 max=999 required=""></div>
+                        <div class="mb-3"><input class="form-control" type="number" name="price" placeholder="Price" min=1 max=999 required=""></div>
                         <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Update Product</button></div>
                     </form>
                 </div>
@@ -152,7 +152,7 @@
                     <p>Product Information</p>
                     <form class="text-center" action="functions/stock-in.php" method="post">
                         <input type="hidden" name="product_id">
-                        <div class="mb-3"><input class="form-control" type="text" name="qty" placeholder="Quantity" required></div>
+                        <div class="mb-3"><input class="form-control" type="text" name="qty" placeholder="Quantity" pattern="^(?:[1-9][0-9]{0,2})$" min=1 max=999 required></div>
                         <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Stock In</button></div>
                     </form>
                 </div>
@@ -170,7 +170,7 @@
                     <p>Product Information</p>
                     <form class="text-center" action="functions/stock-out.php" method="post">
                         <input type="hidden" name="product_id">
-                        <div class="mb-3"><input class="form-control" type="text" name="qty" placeholder="Quantity" required></div>
+                        <div class="mb-3"><input class="form-control" type="text" name="qty" min=1 max=999 placeholder="Quantity" pattern="^(?:[1-9][0-9]{0,2})$" required></div>
                         <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Stock out</button></div>
                     </form>
                 </div>
