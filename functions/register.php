ef5a0ab59f4f917b8f2f0d6febad7ed2381a7dfa
Jccc07 <142872739+Jccc07@users.noreply.github.com>
2024-07-08 18:34:16 +0800

    Regex for Password

--- functions/register.php
+++ functions/register.php
@@ -8,7 +8,13 @@ $username = sanitize_input($_POST['username']);
 $password = sanitize_input($_POST['password']);
 $password_repeat = sanitize_input($_POST['password_repeat']);
 
+$password_regex_special = '/[!@#$%^&*]/';
+$password_regex_letter = '/[A-Za-z]/';
+$password_regex_number = '/\d/';
+$password_regex_length = '/^.{8,}$/';
+
 $errors = array(); // holds all errors in input
+$validation = array(); //holds all validation errors
 
 // Check if the passwords match
 if ($password != $password_repeat) {
@@ -35,11 +41,39 @@ if (strlen($username) < 4) {
     $errors[] = "Username must be at least 4 characters long!";
 }
 
+// Checks if password is valid
+if (!preg_match($password_regex_letter, $password_repeat)) {
+    $validation[] = "Passwords should have at least 1 letter!";
+}
+
+if (!preg_match($password_regex_special, $password_repeat)) {
+    $validation[] = "Passwords should have at least 1 special character!";
+}
+
+if (!preg_match($password_regex_number, $password_repeat)) {
+    $validation[] = "Passwords should have at least 1 number!";
+}
+
+if (!preg_match($password_regex_length, $password_repeat)) {
+    $validation[] = "Passwords should have at least 8 characters!";
+}
+
 // Check if encountered any error
 if (!empty($errors)) {
     $get_str = "Location: ../register.php?username=" . $username;
 
     foreach ($errors as $i => $error) {
+        $get_str .= '&pw_error' . $i . '=' . $error;
+    }
+
+    header($get_str);
+    exit();
+}
+else if (!empty($validation)){
+
+    $get_str = "Location: ../register.php?username=" . $username;
+
+    foreach ($validation as $i => $error) {
         $get_str .= '&pw_error' . $i . '=' . $error;
     }
 
