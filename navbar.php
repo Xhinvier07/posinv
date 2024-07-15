f0d3003a6713014ce512c5fe29898f39e9d20f13
d-quint <112879320+d-quint@users.noreply.github.com>
2024-07-15 16:54:12 +0800

    update design

---
+++ navbar.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+  <div class="container-fluid">
+    <span>SARI | POINT OF SALE AND INVENTORY MANAGEMENT SYSTEM</span>
+      <a href="dashboard.php">
+    <span >
+    <img class="profile" src="assets/img/prof.png" width="50" height="50"></span></a> 
+  </div>
+</nav>
